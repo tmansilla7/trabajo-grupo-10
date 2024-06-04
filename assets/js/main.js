ca456e1348b6da39852aeee90ed33555a649e896
Mariel <marielmarmol1511@outlook.com>
2024-06-04 13:28:21 -0300

    estilos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,35 @@
 const nombreUsuario = document.querySelector("#nombre");
 const password = document.querySelector("#password");
 const submit = document.querySelector("#btn-submit");
-const errorNombre = document.querySelector("#errorNombre")
-const errorPassword = document.querySelector("#errorPassword")
+const errorNombre = document.querySelector("#errorNombre");
+const errorPassword = document.querySelector("#errorPassword");
 
 function removerClaseErrorDelNombre(evento) {
   nombreUsuario.classList.remove("error");
-  errorNombre.textContent = ""
+  errorNombre.textContent = "";
 }
 
 function removerClaseErrorDePassword(evento) {
   password.classList.remove("error");
-  errorPassword.textContent = ""
+  errorPassword.textContent = "";
 }
 
 function verificarFormulario(evento) {
   if (nombreUsuario.value == "") {
-    evento.preventDefault()
+    evento.preventDefault();
     nombreUsuario.classList.add("error");
-    errorNombre.textContent = "Ingresa el nombre de usuario"
-   }
+    errorNombre.textContent = "Ingresa el nombre de usuario";
+  } else {
+    localStorage.setItem("usuario", nombreUsuario.value);
+  }
 
   if (password.value == "") {
-    evento.preventDefault()
+    evento.preventDefault();
     password.classList.add("error");
-    errorPassword.textContent = "Ingresa la contraseña"
+    errorPassword.textContent = "Ingresa la contraseña";
   }
 }
 
 submit.addEventListener("click", verificarFormulario);
 nombreUsuario.addEventListener("keyup", removerClaseErrorDelNombre);
 password.addEventListener("keyup", removerClaseErrorDePassword);
-
