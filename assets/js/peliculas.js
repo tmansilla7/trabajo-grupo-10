7893fed4dd9371753c52650ac2693c494abc07ac
Mariel <marielmarmol1511@outlook.com>
2024-06-09 16:49:43 -0300

    funciones

--- assets/js/peliculas.js
+++ assets/js/peliculas.js
@@ -1,26 +1,9 @@
-const JSON_PELICULAS = localStorage.getItem(LOCAL_STORAGE_SERIES_Y_PELICULAS);
-const PELICULAS = JSON.parse(JSON_PELICULAS);
-
-function agregarSeries() {
-  let nodo_section = document.querySelector(".sermov");
-
-  for (let i in PELICULAS) {
-    if (PELICULAS[i]["tipo"] == "película") {
-      let nodo_article = document.createElement("article");
-      nodo_article.classList.add("frame");
-      let nodo_a = document.createElement("a");
-      nodo_a.href = `./serie-pelicula.html?indice=${i}`;
-      let nodo_img = document.createElement("img");
-      nodo_img.src = PELICULAS[i]["imagen"]["src"];
-      nodo_img.alt = PELICULAS[i]["imagen"]["alt"];
-      let nodo_h3 = document.createElement("h3");
-      nodo_h3.textContent = PELICULAS[i]["titulo"];
-      nodo_a.appendChild(nodo_img);
-      nodo_a.appendChild(nodo_h3);
-      nodo_article.appendChild(nodo_a);
-      nodo_section.appendChild(nodo_article);
+function agregarPeliculas() {
+  for (let i in SERIES_Y_PELICULAS) {
+    if (SERIES_Y_PELICULAS[i]["tipo"] == "película") {
+      crearArticle(i)
     }
   }
 }
 
-agregarSeries();
+agregarPeliculas();
--- assets/js/series.js
+++ assets/js/series.js
@@ -1,24 +1,7 @@
-const JSON_SERIES = localStorage.getItem(LOCAL_STORAGE_SERIES_Y_PELICULAS);
-const SERIES = JSON.parse(JSON_SERIES);
-
 function agregarSeries() {
-  let nodo_section = document.querySelector(".sermov");
-
-  for (let i in SERIES) {
-    if (SERIES[i]["tipo"] == "serie") {
-      let nodo_article = document.createElement("article");
-      nodo_article.classList.add("frame");
-      let nodo_a = document.createElement("a");
-      nodo_a.href = `./serie-pelicula.html?indice=${i}`;
-      let nodo_img = document.createElement("img");
-      nodo_img.src = SERIES[i]["imagen"]["src"];
-      nodo_img.alt = SERIES[i]["imagen"]["alt"];
-      let nodo_h3 = document.createElement("h3");
-      nodo_h3.textContent = SERIES[i]["titulo"];
-      nodo_a.appendChild(nodo_img);
-      nodo_a.appendChild(nodo_h3);
-      nodo_article.appendChild(nodo_a);
-      nodo_section.appendChild(nodo_article);
+  for (let i in SERIES_Y_PELICULAS) {
+    if (SERIES_Y_PELICULAS[i]["tipo"] == "serie") {
+      crearArticle(i)
     }
   }
 }
