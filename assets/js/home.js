0355890183a2144abdc15ced440a3b95e53ee95f
Mariel <marielmarmol1511@outlook.com>
2024-06-10 09:33:35 -0300

    cambio de categoría

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,7 +1,23 @@
 function agregarSeriesYPeliculas() {
   for (let i in SERIES_Y_PELICULAS) {
-    crearArticle(i)
+    crearArticle(i);
   }
 }
 
 agregarSeriesYPeliculas();
+
+CATEGORIAS.addEventListener("change", (event) => {
+  categoria_seleccionada = CATEGORIAS.options[CATEGORIAS.selectedIndex].text;
+
+  nodo_section.innerHTML = "";
+
+  if (categoria_seleccionada == "Todas") {
+    agregarSeriesYPeliculas();
+  } else {
+    for (let i in SERIES_Y_PELICULAS) {
+      if (categoria_seleccionada == SERIES_Y_PELICULAS[i]["categoría"]) {
+        crearArticle(i);
+      }
+    }
+  }
+});
